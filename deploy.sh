0799ce854ceeb628a7b63965a2bcbeefc5fa804a
Gaspar Hermán <gaspar.herman@totoro.safelayer.lan>
2019-01-24 15:37:58 +0100

    Eliminado cat del properties después de cambios

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,6 @@ if [[ "$TRAVIS_BRANCH" = 'master' ]]; then
 	sed -i "s/REPLACE_BY_SONATYPE_PASSWORD/${SONATYPE_PASSWORD}/g" gradle.properties
 	sed -i "s/REPLACE_BY_GPG_KEY_ID/${GPG_KEY_ID}/g" gradle.properties
 	sed -i "s/REPLACE_BY_GPG_KEY_PASSPHRASE/${GPG_KEY_PASSPHRASE}/g" gradle.properties
-
-	cat gradle.properties
-
+	
 	gradle clean uploadArchives
 fi
