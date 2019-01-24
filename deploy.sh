06d21c3345d6e949f2b2f83c61381832fd26b63a
Gaspar Hermán <gaspar.herman@totoro.safelayer.lan>
2019-01-24 11:30:53 +0100

    Travis CI prueba II

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ]; then
 	openssl aes-256-cbc -K $encrypted_9558564ad590_key -iv $encrypted_9558564ad590_iv -in secret.gpg.enc -out secret.gpg -d
-	gradle clean uploadArchvies -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=secret.gpg -Prelease=${release}
+	gradle clean uploadArchvies -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=secret.gpg
 fi
